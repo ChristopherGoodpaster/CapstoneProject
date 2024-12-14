5c5241a476fc7969c63c06fc43662a752284deb5
Christopher Goodpaster <chris.goodpaster83@gmail.com>
2024-12-14 14:05:40 -0500

    fixed requirements.txt

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="YourProjectName",
+    version="1.0",
+    install_requires=[
+        "pandas",
+        "matplotlib",
+        "seaborn",
+        "mplcursors",
+        "requests",
+        "beautifulsoup4"
+    ],
+)
